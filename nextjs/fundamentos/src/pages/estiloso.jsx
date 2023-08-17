adc8fb2caaea5cd353d7a3d73e96ace67f9d6f51
Jean Henrique Schvartz <98992157+Schvras@users.noreply.github.com>
2023-08-17 00:45:06 +0000

    Primeiro commit

---
+++ nextjs/fundamentos/src/pages/estiloso.jsx
@@ -0,0 +1,51 @@
+export default function Estiloso(){
+    return (
+        <div>
+            <h1>Currículo</h1>
+            <hr />
+            <p><b>Nome:</b> Jean Henrique Schvartz</p>
+            <p><b>Objetivo:</b> Desenvolvedor Web React.js</p>
+            <p><b>Data nascimento:</b> 11/02/2001</p>
+            <p><b>Contato:</b> henrique.jeanschvartz@gmail.com</p>
+            <hr />
+            <h2>Qualificações profissionais</h2>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Genexus</li>
+                <li>PostreSQL</li>
+            </ul>
+            <hr />
+            <h2>Experiências profissionais</h2>
+            <div>
+                <p><b>Siscobra Sistema</b></p>
+                <ul>
+                    <li>Desenvolvedor Web Pleno</li>
+                    <li>Desenvolvedor Web Junior</li>
+                    <li>Suporte técnico</li>
+                </ul>
+                <p><b>jan/2022 - atual</b></p>
+            </div>
+            <div>
+                <p><b>Brandili Malhas</b></p>
+                <ul>
+                    <li>Auxiliar contábil</li>
+                    <li>Revisor de malhas</li>
+                </ul>
+                <p><b>mar/2019 - dez/2022</b></p>
+            </div>
+            <div>
+                <p><b>Brandili Têxtil</b> Jovem Aprendiz</p>
+                <p>Realizava o controle de estoque local, separação de amostras e também trabalhos manuais.</p>
+                <p><b>jan/2017 - dez/2018</b></p>
+            </div>
+            <hr />
+            <h2>Educação</h2>
+            <p>Cibersistemas para automação - SENAI</p>
+            <p>Aprendizagem indústrial - SENAI</p>
+            <p>Ensino médio completo</p>
+        </div>
+    )
+}
---
+++ nextjs/fundamentos/src/pages/jsx.jsx
@@ -0,0 +1,17 @@
+export default function Jsx(){
+    const titulo = <h1>JSX é um conceito central</h1>
+
+    function subtitulo(){
+        return <h2>{"muito legal".toLocaleUpperCase()}</h2>
+    }
+
+    return(
+        <div>
+            {titulo}
+            {subtitulo()}
+            <p>
+                {JSON.stringify({nome: 'Jean', idade: 22})}
+            </p>
+        </div>
+    )
+}
