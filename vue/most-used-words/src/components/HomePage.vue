3ec57fad28ab26ae2ef777c6bdc38155c5b7ea8e
Jean Henrique <henrique.jeanschvartz@gmail.com>
2023-10-22 14:34:31 -0300

    vue most-used-words

---
+++ vue/most-used-words/src/components/HomePage.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="home">
+        <v-form>
+            <v-file-input
+                label="Selecione os arquivos de legendas"
+                prepend-icon="mdi-file"
+                append-icon="mdi-send"
+                extension="srt"
+                outlined
+                multiple
+                v-model="files"
+                @click:append="proccessSubtitles"
+            >
+            </v-file-input>
+        </v-form>
+        <div class="pills">
+            <PillComponent v-for="item in groupedWords" :word="item.word" :amount="item.amount" :key="item.word"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import PillComponent from './PillComponent.vue'
+
+export default {
+    name: "HomePage",
+    data: () => ({
+        groupedWords: [
+            {word: "i", amount: 500},
+            {word: "you", amount: 478},
+            {word: "it", amount: 10},
+        ],
+        files: null
+    }),
+    components: {
+        PillComponent
+    },
+    methods: {
+        proccessSubtitles(){
+            console.log(this.files)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .home{
+        margin: 20px
+    }
+    .pills{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center
+    }
+</style>
---
+++ vue/most-used-words/src/components/PillComponent.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="pill">
+    <span class="word">{{word}}</span>
+    <span class="amount">{{amount}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "PillComponent",
+    props: ["word","amount"]
+}
+</script>
+
+<style scoped>
+    .pill{
+        margin: 10px;
+        height: 55px;
+        border-radius: 35px;
+        color: white;
+        font-size: 1.3rem;
+        font-weight: bold;
+        display: flex;
+        align-items: center;
+        background-color: red;
+        width: fit-content;
+    }
+
+    .pill .word{
+        margin-left: 15px;
+    }
+
+    .pill .amount{
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        background-color: white;
+        color: red;
+        margin: 10px;
+        padding: 5px;
+        font-size: 14px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
